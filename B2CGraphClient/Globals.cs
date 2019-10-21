31253aa37ee96352b97265d5f1bcb735207f70a8
Anduin2017 <anduin@aiursoft.com>
2019-10-21 16:21:44 +0900

    Upgrade to .NET Core 3.0

--- B2CGraphClient/Globals.cs
+++ B2CGraphClient/Globals.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace B2CGraphShell
+﻿namespace B2CGraphShell
 {
     public static class Globals
     {
